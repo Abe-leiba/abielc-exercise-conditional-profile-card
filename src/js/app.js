e2a4da35f5524ab2bae3215e9b092926ae4b4dfd
Abe-leiba <abel_con@yahoo.com>
2025-08-25 22:04:56 +0000

    abiels unfinished conditional profile card

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,4 @@
+import { left } from "@popperjs/core";
 import "../style/index.css";
 
 /**
@@ -33,11 +34,24 @@ function render(variables = {}) {
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
+          <h1>
+          ${variables.name ? variables.name : `first`}
+          ${variables.lastName ? variables.lastName : `last`}
+        </h1>
+          <h2>${variables.role ? variables.role : `role`}</h2>
+          <h3>${variables.city ? variables.city : `city`},
+          ${variables.country ? variables.country : `country`}</h3>
+          <ul class=${
+            variables.socialMediaPosition == "position-left"
+              ? "position-left"
+              : "position-right"
+          } >
+            <li>
+
+              
+              <a href="https://twitter.com/${
+                variables.instagram
+              }"><i class="fab fa-twitter"></i></a></li>
             <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
             <li><a href="https://linkedin.com/school/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
             <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
