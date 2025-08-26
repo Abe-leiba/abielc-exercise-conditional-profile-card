d11e2fd33581ac33d7f5bc12238cb690d70da406
Abe-leiba <abel_con@yahoo.com>
2025-08-26 22:54:21 +0000

    better late than never conditional profile card

--- src/js/app.js
+++ src/js/app.js
@@ -41,20 +41,31 @@ function render(variables = {}) {
           <h2>${variables.role ? variables.role : `role`}</h2>
           <h3>${variables.city ? variables.city : `city`},
           ${variables.country ? variables.country : `country`}</h3>
-          <ul class=${
+          <ul class="${
             variables.socialMediaPosition == "position-left"
               ? "position-left"
               : "position-right"
-          } >
-            <li>
+          } ">
 
-              
-              <a href="https://twitter.com/${
-                variables.instagram
-              }"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/school/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+        
+            <li><a href="https://twitter.com/${
+              variables.twitter
+            }"><i class="fab fa-twitter"></i><a/></li>
+            
+          
+
+             <li>
+            <a href="https://github.com/${
+              variables.github
+            }"><i class="fab fa-github"></i></a></li>
+
+            <li><a href="https://linkedin.com/school/${
+              variables.linkedin
+            }"><i class="fab fa-linkedin"></i></a></li>
+
+            <li><a href="https://instagram.com/${
+              variables.instagram
+            }"><i class="fab fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
